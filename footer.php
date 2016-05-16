5aa7670fcca7708ddcf993830693e9a593d917c6
Bitcodeca <bitcodemail@gmail.com>
2016-05-16 15:47:46 -0400

    Modificacion del footer, mostrando en doble columna los repuestos por marca

--- footer.php
+++ footer.php
@@ -1,84 +1,68 @@
 	<div class="clearfix"></div>
+    <?php 
+        $args = array('hide_empty' => false); $countgrupo=wp_count_terms( 'grupo', $args );
+        $args = array('hide_empty' => false); $countmarca=wp_count_terms( 'marca', $args );
+        $colgrupo=$countgrupo/2;
+        $colmarca=$countmarca/2;
+        if ($colgrupo>$colmarca) {
+            $x=$colgrupo;
+        } else { $x=$colmarca; }
+    ?>
         <footer>
             <div class="row margintop25 marginbot10">
                 <div class="container">
-                    <div class="col-md-3 col-sm-3 col-xs-12">
+                    <div class="col-md-3 col-sm-6 col-xs-12">
                         <div class="titulofooter">
                             <h3 class="margin0 letra2">Contáctanos</h3>
                         </div>
-                        <h5><i class="margintop10 fa fa-phone fa-lg"> </i> Teléfono: (058) 0251-xxxxxxx</h5>
+                        <h5 class="letra2"><i class="margintop10 fa fa-phone fa-lg"> </i> Teléfono: (058) 0251-xxxxxxx</h5>
                         <h5 class="marginbot0 letra2"><i class="fa fa-fax fa-lg"> </i> Fax: (058) 0251-xxxxxxx</h5>
                         <h5 class="marginbot0 letra2"><i class="fa fa-map-marker fa-lg"></i> Barquisimeto, Edo Lara. Venezuela </h5>
                         <h5 class="marginbot0 letra2"><i class="fa fa-envelope"></i>email</h5>
                         <h5 class="marginbot0 letra2"><i class="fa fa-envelope"></i> fax</h5>
                         <h5 class="marginbot0 letra2"><i class="fa fa-envelope"></i> telf</h5>
                     </div>
-                    <div class="col-md-3 col-sm-3 col-xs-12">
+                    <div class="col-md-3 col-sm-6 col-xs-12">
                         <div class="titulofooter">
-                            <h3 class="margin0 letra2">Nuestra Oficina</h3>
-                        </div>
-                        <div class="margintop10 paddingtop1">
-                            <?php $marca = get_terms( 'marca', array( 'orderby' => 'name', 'hide_empty' => 1 ) );
-                            foreach ($marca as $i) {
-                                echo '<h5 class="marginbot0 letra2">'.$i->name.'</option>';
-                             } ?>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 col-sm-3 col-xs-12">
-                        <div class="titulofooter">
-                            <h3 class="margin0 letra2">Categorías</h3>
+                            <h3 class="margin0 letra2">Productos</h3>
                         </div>
                         <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/" class="margintop10">
                             <label class="hidden" for="s"><?php _e('Search:'); ?></label>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Carburadores y sus Partes">
-                                <h5 class="marginbot0 letra2">Carburadores y sus Partes</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Partes eléctricas">
-                                <h5 class="marginbot0 letra2">Partes Eléctricas</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s"  value="Fuel Injection">
-                                <h5 class="marginbot0 letra2">Fuel Injection</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Sensores">
-                                <h5 class="marginbot0 letra2">Sensores</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Kit de Tiempo y Entonación">
-                                <h5 class="marginbot0 letra2">Kit de Tiempo y Entonación</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Aire Acondicionado">
-                                <h5 class="marginbot0 letra2">Aire Acondicionado</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Carrocería">
-                                <h5 class="marginbot0 letra2">Carrocería</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Suspensión">
-                                <h5 class="marginbot0 letra2">Suspensión</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Tren Delantero">
-                                <h5 class="marginbot0 letra2">Tren Delantero</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Frenos">
-                                <h5 class="marginbot0 letra2">Frenos</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Rodamiento">
-                                <h5 class="marginbot0 letra2">Rodamiento</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Accesorios">
-                                <h5 class="marginbot0 letra2">Accesorios</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Correas y Mangueras">
-                                <h5 class="marginbot0 letra2">Correas y Mangueras</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Transmisión">
-                                <h5 class="marginbot0 letra2">Transmisión</h5>
-                            </button>
-                            <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="Otros">
-                                <h5 class="marginbot0 letra2">Otros</h5>
-                            </button>
+                            <?php $marca = get_terms( 'grupo', array( 'orderby' => 'name', 'hide_empty' => 0 ) );
+                            foreach ($marca as $i) { ?>
+                                    <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="<?php echo $i->slug; ?>">
+                                        <h5 class="marginbot0 letra2"><?php echo $i->name; ?></h5>
+                                    </button>
+                            <?php } ?>
                         </form>
                     </div>
-                    <div class="col-md-3 col-sm-3 col-xs-12">
+                    <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="titulofooter">
+                            <h3 class="margin0 letra2">Repuestos por Marca</h3>
+                        </div>
+                        <div class="margintop10 paddingtop1">
+                            <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/" >
+                                <label class="hidden" for="s"><?php _e('Search:'); ?></label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                <?php $marca = get_terms( 'marca', array( 'orderby' => 'name', 'hide_empty' => 1 ) );
+                                $y=0;
+                                foreach ($marca as $i) { 
+                                if ($y<=$x) { ?>
+                                    <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="<?php echo $i->slug; ?>">
+                                        <h5 class="marginbot0 letra2"><?php echo $i->name; ?></h5>
+                                    </button>
+                                <?php } else { 
+                                    $y=0; ?>
+                                    </div>
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                <?php }
+                                    $y++;
+                                } ?>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12">
                         <div class="titulofooter">
                             <h3 class="margin0 letra2">Mapa del Sitio</h3>
                         </div>
--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 	<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/" class="margintop10">
 	<label class="hidden" for="s"><?php _e('Search:'); ?></label>
 		<div class="titulosidebar">
-			<h3 class="margin0 letra2">Selecciona tu vehículo</h3>
+			<h3 class="margin0 letra2">Filtrar por modelo</h3>
 		</div>
 		<div class="margintop15">
 			<select class="btn sidebar-option letra2" id="marca" name="marca">
@@ -32,54 +32,15 @@
 	<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/" class="margintop25">
 	<label class="hidden" for="s"><?php _e('Search:'); ?></label>
 		<div class="titulosidebar">
-			<h3 class="margin0 letra2">Categorías</h3>
+			<h3 class="margin0 letra2">Filtrar por Categoría</h3>
 		</div>
 		<div class="categoriassidebar">
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Carburadores y sus Partes">
-				<h4 class="letra2">Carburadores y sus Partes</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Partes eléctricas">
-				<h4 class="letra2">Partes Eléctricas</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s"  value="Fuel Injection">
-				<h4 class="letra2">Fuel Injection</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Sensores">
-				<h4 class="letra2">Sensores</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Kit de Tiempo y Entonación">
-				<h4 class="letra2">Kit de Tiempo y Entonación</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Aire Acondicionado">
-				<h4 class="letra2">Aire Acondicionado</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Carrocería">
-				<h4 class="letra2">Carrocería</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Suspensión">
-				<h4 class="letra2">Suspensión</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Tren Delantero">
-				<h4 class="letra2">Tren Delantero</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Frenos">
-				<h4 class="letra2">Frenos</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Rodamiento">
-				<h4 class="letra2">Rodamiento</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Accesorios">
-				<h4 class="letra2">Accesorios</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Correas y Mangueras">
-				<h4 class="letra2">Correas y Mangueras</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Transmisión">
-				<h4 class="letra2">Transmisión</h4>
-			</button>
-			<button class="btn btn-search displayblock" type="submit" name="s" id="s" value="Otros">
-				<h4 class="letra2">Otros</h4>
-			</button>
+			<?php $marca = get_terms( 'grupo', array( 'orderby' => 'name', 'hide_empty' => 0 ) );
+            foreach ($marca as $i) { ?>
+                <button class="btn btn-footer displayblock" type="submit" name="s" id="s" value="<?php echo $i->slug; ?>">
+                    <h4 class="letra2"><?php echo $i->name; ?></h4>
+                </button>
+            <?php } ?>
 		</div>
 	</form>
 
